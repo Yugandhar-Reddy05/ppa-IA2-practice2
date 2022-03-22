d2cad749a21fa4f90dedf12c47784bbfe70572f1
C.v.s Yugandhar <93091593+Yugandhar-Reddy05@users.noreply.github.com>
2022-03-22 04:14:20 +0000

    done

--- p1original.c
+++ p1original.c
@@ -1,10 +1,27 @@
 #include<stdio.h>
+void input(float *base,float *height)
+{
+  printf("Enter the base length: \n");
+  scanf("%f",base);
+  printf("Enter the height length: \n");
+  scanf("%f",height);
+  
+}
+
+void find_area(float base,float height,float *area)
+{
+  *area=0.5*(base)*(height);
+}
+void output(float base,float height,float area)
+{
+  printf("The area of %0.3f and %0.3f is %0.3f", base,height,area);
+}
+
 int main()
 {
-    float base,height,*area;
-    printf("enter the base,height values\n");
-    scanf("%f%f",&base,&height);
-    *area = ((base+height)/2);
-    printf("the area of trinagle =%f",*area);
-    return 0;
+  float base,height,area;
+  input(&base,&height);
+  find_area(base,height,&area);
+  output(base,height,area);
+  return 0;
 }
---
+++ p3original.c
@@ -0,0 +1,53 @@
+int input_number();
+int is_composite(int n);
+void output(int n, int composite);
+input: 
+8
+output:
+8 is a composite number.
+
+#include<stdio.h>
+int input_number()
+{
+  int n;
+  printf("Enter a number: \n");
+  scanf("%d",&n);
+  return n;
+}
+int is_composite(int n)
+{
+  for(int i=2;i<n/2;i++)
+    {
+      int result=n%i;
+      if(result==0)
+      {
+        return 0;
+      }
+      if(result!=0)
+      {
+        return 1;
+      }
+    return result;
+    }
+}
+void output(int n,int composite)
+{
+  int result;
+  if(result==0)
+  {
+    printf("The number %d is composite",n);
+  }
+  if(result==1)
+  {
+    printf("The number is a prime number");
+  }
+}
+int main()
+{
+  int n,composite;
+  n=input_number();
+  composite=is_composite(n);
+  output(n,composite);
+  return 0;
+}
+
